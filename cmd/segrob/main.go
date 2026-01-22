8b251abdd1bcda776bdd5b1c6c965cb616f55db7
revelaction <96388231+revelaction@users.noreply.github.com>
2026-01-22 18:35:08 +0100

    docs: fix duplicate comment in main.go

--- cmd/segrob/main.go
+++ cmd/segrob/main.go
@@ -64,12 +64,7 @@ func runCommand(cmd string, args []string, ui UI) error {
     // handle custom keybinds and colors) but fails to restore it to Cooked
     // Mode (canonical mode) upon exit. When the terminal is left in Raw Mode,
     // it often disables local echo (typing is invisible) and carriage
-    // returns.The issue occurs because go-prompt puts your terminal into Raw
-    // Mode (to handle custom keybinds and colors) but fails to restore it to
-    // Cooked Mode (canonical mode) upon exit. When the terminal is left in Raw
-    // Mode, it often disables local echo (typing is invisible) and carriage
-    // returns.
-    //
+    // returns.    
 	// For interactive commands, we save the terminal state (Cooked Mode)
 	// and strictly restore it when the function returns.
 	if cmd == "query" || cmd == "edit" {
